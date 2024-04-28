fb630f8038bd9ce01b550ddcf5b01a9894248f2a
hermessil <Silas.Hermes02@gmail.com>
2024-04-28 15:34:01 +0200

    add TODO in function get_data_from_website

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import json
+import time
+
+import requests
+import selenium
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from time import sleep
 
 
 def read_data_from_config() -> str:
@@ -9,5 +17,18 @@ def read_data_from_config() -> str:
     return website
 
 
+def get_data_from_website(website: str):
+    """Get Data (Sudoku Grid values) from website"""
+    #TODO: setup accept cookie popup
+    """
+    # chrome_path = 'C:\Program Files\Google\Chrome\Application'
+    driver = webdriver.Chrome()
+    driver.get(website)
+    time.sleep(5)
+    elem = driver.find_element(By.XPATH, '//*[@id="notice"]/div[4]/div[1]/button').click()
+    """
+
+
 if __name__ == "__main__":
     website_url = read_data_from_config()
+    print(get_data_from_website(website_url))
