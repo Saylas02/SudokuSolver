d30dd4c9ee3f1cafb1cc4ba687e3882fe2cfdf4f
hermessil <Silas.Hermes02@gmail.com>
2024-05-01 16:57:36 +0200

    set up function check_blocks()

--- solver.py
+++ solver.py
@@ -29,8 +29,26 @@ def check_columns(grid: list, arr_pos_nr: list):
 
 
 def check_blocks(grid: list, arr_pos_nr: list):
-    for x, y in sudoku_matrix_generator():
+    block_coordinates = {
+        (0, 0): [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)],
+        (0, 1): [(0, 3), (0, 4), (0, 5), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5)],
+        (0, 2): [(0, 6), (0, 7), (0, 8), (1, 6), (1, 7), (1, 8), (2, 6), (2, 7), (2, 8)],
 
+        (1, 0): [(3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)],
+        (1, 1): [(3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)],
+        (1, 2): [(3, 6), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8), (5, 6), (5, 7), (5, 8)],
+
+        (2, 0): [(6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 0), (8, 1), (8, 2)],
+        (2, 1): [(6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5)],
+        (2, 2): [(6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8)],
+    }
+    for y, x in sudoku_matrix_generator():
+        if grid[y][x] != 0:
+            continue
+        block_y, block_x = (y//3), (x//3)
+        for bar, foo in block_coordinates[(block_y, block_x)]:
+            if grid[bar][foo] != 0 and grid[bar][foo] in arr_pos_nr[y][x]:
+                arr_pos_nr[y][x].remove(grid[bar][foo])
     return
 
 
@@ -60,4 +78,5 @@ if __name__ == "__main__":
     pretty_print_sudoku(board)
     check_rows(board, possible_nr)
     check_columns(board, possible_nr)
+    check_blocks(board, possible_nr)
     print(possible_nr)
